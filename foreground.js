7a4a05a000ff5f44434936767633c58aa7648344
MrWarzo <pereira@winylo.com>
2023-10-31 15:13:43 +0100

    Suppression consolelog debug

--- foreground.js
+++ foreground.js
@@ -1,11 +1,3 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
-
-console.log("This prints to the console of the page (injected only if the page url matched)");
-
 window.addEventListener("load", () => main());
 
 
@@ -17,9 +9,7 @@ function main() {
             const total = pNode.innerHTML;
 
             const totalDuration = total.match(/([0-1]?[0-9]|2[0-3])h ([0-5][0-9])/);
-            console.log("totalDuration1", totalDuration[1], "totalDuration2", totalDuration[2]);
             const totalTimestamp = (parseInt(totalDuration[1]) * 60 + parseInt(totalDuration[2])) * 60 * 1000;
-            console.log("totalTimestamp", totalTimestamp);
 
             const hour = title.match(/([0-1]?[0-9]|2[0-3]):([0-5][0-9])/);
 
@@ -32,12 +22,9 @@ function main() {
                 const now = new Date();
 
                 const calculated = now.getTime() - date.getTime();
-                console.log("calculated", calculated);
 
                 const newTotalTimestamp = calculated + totalTimestamp;
                 const newHour = [Math.floor(newTotalTimestamp / 1000 / 60 / 60), Math.floor(newTotalTimestamp / 1000 / 60 % 60)];
-                console.log("newTotalTimestamp", newTotalTimestamp);
-                console.log("newHour", newHour);
 
                 const trueTotalPNode = document.createElement('p');
                 trueTotalPNode.innerHTML = `<span style="font-weight:600">Vrai total : </span>` + newHour[0] + "h " + newHour[1];
